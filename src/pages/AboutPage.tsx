f11a6a027e5a5562e0eebbebb7ae1a03dc1abbc0
Ahmed <m.ahmed.fahim02@gmail.com>
2026-06-12 11:52:47 +0500

    add leadership page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import {
@@ -244,15 +245,11 @@ export default function AboutPage() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="text-center">
-              {/* <a
-                href="https://www.collectiveip.co.uk/meet-the-team/"
-                target="_blank"
-                rel="noopener noreferrer"
-              > */}
-              <div className="w-24 h-24 bg-[#937bbd] rounded-full mx-auto mb-4 flex items-center justify-center">
-                <Users className="text-[#70309d]" size={32} />
-              </div>
-              {/* </a> */}
+              <Link to="/leadership">
+                <div className="w-24 h-24 bg-[#937bbd] rounded-full mx-auto mb-4 flex items-center justify-center cursor-pointer hover:scale-105 transition">
+                  <Users className="text-[#70309d]" size={32} />
+                </div>
+              </Link>
               <h3 className="font-heading text-lg font-bold text-[#3B4041] mb-2">
                 Leadership Team
               </h3>
